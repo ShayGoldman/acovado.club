43a1d043ea17e44a3fa55c4c59c3e1942c1a4ffe
Shay Goldman <shay.goldmanz@gmail.com>
2025-01-14 21:48:13 +0200

    chore(rename main schema): rename database schema to acovado

--- modules/db/src/migrations/20250114193127_common_leader.sql
+++ modules/db/src/migrations/20250114193127_common_leader.sql
@@ -1,8 +1,8 @@
-CREATE SCHEMA "finance";
+CREATE SCHEMA "acovado";
 --> statement-breakpoint
 CREATE SCHEMA "metabase";
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."collections" (
+CREATE TABLE IF NOT EXISTS "acovado"."collections" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"type" varchar(64) NOT NULL,
 	"status" varchar(64) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS "finance"."collections" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."kv_store" (
+CREATE TABLE IF NOT EXISTS "acovado"."kv_store" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"key" varchar(128) NOT NULL,
 	"value" varchar(256) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS "finance"."kv_store" (
 	CONSTRAINT "kv_store_key_unique" UNIQUE("key")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."signal_metrics" (
+CREATE TABLE IF NOT EXISTS "acovado"."signal_metrics" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"ticker_id" uuid NOT NULL,
 	"collection_id" integer NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS "finance"."signal_metrics" (
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."stories" (
+CREATE TABLE IF NOT EXISTS "acovado"."stories" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"type" varchar(128) NOT NULL,
 	"ticker_id" uuid NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS "finance"."stories" (
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."tickers" (
+CREATE TABLE IF NOT EXISTS "acovado"."tickers" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(64) NOT NULL,
 	"symbol" varchar(8) NOT NULL,
@@ -47,14 +47,14 @@ CREATE TABLE IF NOT EXISTS "finance"."tickers" (
 	CONSTRAINT "tickers_symbol_unique" UNIQUE("symbol")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."watch_list_to_tickers" (
+CREATE TABLE IF NOT EXISTS "acovado"."watch_list_to_tickers" (
 	"watch_list_id" uuid NOT NULL,
 	"ticker_id" uuid NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "watch_list_to_tickers_watch_list_id_ticker_id_pk" PRIMARY KEY("watch_list_id","ticker_id")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "finance"."watch_lists" (
+CREATE TABLE IF NOT EXISTS "acovado"."watch_lists" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(128) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
@@ -62,25 +62,25 @@ CREATE TABLE IF NOT EXISTS "finance"."watch_lists" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "finance"."signal_metrics" ADD CONSTRAINT "signal_metrics_ticker_id_tickers_id_fk" FOREIGN KEY ("ticker_id") REFERENCES "finance"."tickers"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "acovado"."signal_metrics" ADD CONSTRAINT "signal_metrics_ticker_id_tickers_id_fk" FOREIGN KEY ("ticker_id") REFERENCES "acovado"."tickers"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "finance"."signal_metrics" ADD CONSTRAINT "signal_metrics_collection_id_collections_id_fk" FOREIGN KEY ("collection_id") REFERENCES "finance"."collections"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "acovado"."signal_metrics" ADD CONSTRAINT "signal_metrics_collection_id_collections_id_fk" FOREIGN KEY ("collection_id") REFERENCES "acovado"."collections"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "finance"."watch_list_to_tickers" ADD CONSTRAINT "watch_list_to_tickers_watch_list_id_watch_lists_id_fk" FOREIGN KEY ("watch_list_id") REFERENCES "finance"."watch_lists"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "acovado"."watch_list_to_tickers" ADD CONSTRAINT "watch_list_to_tickers_watch_list_id_watch_lists_id_fk" FOREIGN KEY ("watch_list_id") REFERENCES "acovado"."watch_lists"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "finance"."watch_list_to_tickers" ADD CONSTRAINT "watch_list_to_tickers_ticker_id_tickers_id_fk" FOREIGN KEY ("ticker_id") REFERENCES "finance"."tickers"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "acovado"."watch_list_to_tickers" ADD CONSTRAINT "watch_list_to_tickers_ticker_id_tickers_id_fk" FOREIGN KEY ("ticker_id") REFERENCES "acovado"."tickers"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
